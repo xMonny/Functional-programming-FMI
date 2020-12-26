c7c8cf541e962558677d0c8733531dd4c7f38f21
xMonny <55389898+xMonny@users.noreply.github.com>
2020-12-26 18:26:02 +0200

    Changed syntax in 02-sum-squares-bigger-two.hs

--- 02-Haskell-Exercises/Exercise-1/02-sum-squares-bigger-two.hs
+++ 02-Haskell-Exercises/Exercise-1/02-sum-squares-bigger-two.hs
@@ -6,12 +6,12 @@ sumSquares a b = a*a + b*b
 
 findMinimum :: Int -> Int -> Int -> Int
 findMinimum a b c
- | a < b && a < c = a
- | b < a && b < c = b
- | c < a && c < b = c
+   | a < b && a < c = a
+   | b < a && b < c = b
+   | c < a && c < b = c
 
 sumSquaresBiggerTwo :: Int -> Int -> Int -> Int
 sumSquaresBiggerTwo a b c
- | findMinimum a b c == a = sumSquares b c --a < b < c
- | findMinimum a b c == b = sumSquares a c --b < a < c
- | otherwise              = sumSquares a b
+   | findMinimum a b c == a = sumSquares b c --a < b < c
+   | findMinimum a b c == b = sumSquares a c --b < a < c
+   | otherwise              = sumSquares a b
